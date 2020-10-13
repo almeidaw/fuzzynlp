04324e0394c1e801fd0302bac496f1abd6590825
William Almeida <william.almeida@usp.br>
2020-10-13 12:06:35 -0300

    brand new code

--- CoreNLP.py
+++ CoreNLP.py
@@ -1,54 +1,104 @@
 from pycorenlp import StanfordCoreNLP
 from senticnet.senticnet import SenticNet
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import os
 import pathlib
-import pandas as pd
 
-#Define local do servidor StanforCoreNLPToolkit
-nlp = StanfordCoreNLP('http://localhost:9000')
 sn = SenticNet()
-
-#Define o caminho para os diretórios dos domínios de treinamento (inModel) e de teste (outModel)
-inModelDir = pathlib.Path(__file__).parent.absolute().joinpath('paper-package/dranziera')
-outModelDir = pathlib.Path(__file__).parent.absolute().joinpath('paper-package/out_of_domain')
-
-#pega o nome de cada diretório dentro do diretório Dranziera
-for dir in os.listdir(inModelDir):
-
-	#transforma esse nome do diretório em uma path para verificar se é um diretório mesmo 
-	#(para evitar de pegar arquivos ocultos do sistema que podem estar dentro do diretório Dranziera)
-	dirPath = os.path.join(inModelDir, dir)
-	if os.path.isdir(dirPath):
-
-		#lista todos os arquivos dentro do diretório do domínio
-		files = os.listdir(dirPath)
-
-		#abre cara arquivo na pasta do domínio para leitura
-		for file in files:
-
-			with open("%s/%s" % (dirPath, file)) as dataset:
-
-				#lê primeira linha do arquivo em questão e itera por cada uma das próximas linhas (cada linha é um review)
-				line = dataset.readline()
-				while line:
-
-					#joga linha toda no Stanford Core NLP Toolkit
-					coreoutput = nlp.annotate(line, properties={'annotators': 'lemma, pos, sentiment', 'outputFormat': 'json', 'timeout': 100000})
-
-					print("\rDOCUMENT: %s" % (line))
-
-					for sentence in coreoutput["sentences"]:
-						
-						print("SENTENCE: %d\n" % (sentence["index"]))
-
-						for token in sentence["tokens"]:
-							#senticnetoutput = sn.concept(line)
-							print("TOKEN: %d, WORD: %s, LEMMA: %s, POS: %s, SENTIMENT: %s" % (token["index"], token["word"], token["lemma"], token["pos"], sentence["sentiment"]))
-						
-						print("\r")
+nlp = StanfordCoreNLP('http://localhost:9000')
+domains = {}
+sumZjc = {}
+domain_df = pd.DataFrame
+trainingDir = pathlib.Path(__file__).parent.absolute().joinpath('dataset-teste')
 
 
-					line = dataset.readline()
+for dir in os.listdir(trainingDir):
+    features = []
+    pos = []
+    peic = []
+    kic = []
+    sic = []
+    zic = []
+    ni = 0
+    difreq = []
+    dirPath = os.path.join(trainingDir, dir)
 
+    if os.path.isdir(dirPath) and dir[0] != ".":
+        files = os.listdir(dirPath)
 
-	#dt.to_csv('CoreNLPOutput.csv’)
+        for file in files:
+            print(file)
+            if "pos" in file:
+                polarity = 1
+            else:
+                polarity = -1
+
+            with open("%s/%s" % (dirPath, file)) as dataset:
+                line = dataset.readline()
+
+                while line:
+                    inDocument = False
+                    coreOutput = nlp.annotate(line,
+                                              properties={'annotators': 'lemma, pos, depparse', 'outputFormat': 'json',
+                                                          'timeout': 100000})
+                    for sentence in coreOutput["sentences"]:
+                        for token in sentence["tokens"]:
+                            if token["pos"] in ["NN", "NNS", "NNP", "NNPS", "VB", "VBD", "VBG", "VBN", "VBP",
+                                                "VBZ", "RB", "RBR", "RBS", "JJS", "JJR", "JJ"]:
+
+                                lemma = token["lemma"]
+
+                                if not(lemma in features):
+                                    features.append(lemma)
+                                    kic.append(polarity)
+                                    sic.append(1)
+                                    zic.append(1)
+                                    pos.append(token["pos"])
+                                    inDocument = True
+
+                                else:
+                                    index = features.index(lemma)
+                                    zic[index] += 1
+                                    if not inDocument:
+                                        kic[index] += polarity
+                                        sic[index] += 1
+                                        inDocument = True
+
+                                if not(lemma in sumZjc):
+                                    sumZjc.update({lemma: 1})
+                                else:
+                                    sumZjc.update({lemma: (sumZjc.get(lemma) + 1)})
+
+                    line = dataset.readline()
+
+        ni = sum(zic)
+
+        for feature in features:
+            index = features.index(feature)
+            peic.append(kic[index] / sic[index])
+            difreq.append(zic[index]/ni)
+
+        domain_df = pd.DataFrame(list(zip(features, pos, kic, sic, peic, zic, difreq)), columns=[
+            "Feature", "POS", "KiC", "SiC", "PeiC", "ZiC", "Di_Freq"])
+        domains.update({dir: domain_df})
+
+for domain, dataframe in domains.items():
+    zjc = []
+    diuniq = []
+    DBDi = []
+    pcs = []
+    for index, row in dataframe.iterrows():
+        zic = row["ZiC"]
+        difreq = row["Di_Freq"]
+        zjc.insert(index, sumZjc.get(row["Feature"]))
+        diuniq.insert(index, zic/zjc[index])
+        DBDi.insert(index, difreq*diuniq[index])
+        # pcs.insert(index, sn.polarity_intense(row["Feature"]))
+
+    dataframe["Sum_Zjc"] = zjc
+    dataframe["Di_Uniq"] = diuniq
+    dataframe["DBDi"] = DBDi
+    # dataframe["SenticNet"] = pcs
+    dataframe.to_csv("outputs/%s.csv" % domain)
--- teste2.py
+++ teste2.py
@@ -1,51 +1,48 @@
 from pycorenlp import StanfordCoreNLP
-#from senticnet.senticnet import SenticNet
+import pandas as pd
 import os
 import pathlib
-import pandas as pd
-import json
 
-#Define local do servidor StanforCoreNLPToolkit
+# Define local do servidor StanforCoreNLPToolkit
 nlp = StanfordCoreNLP('http://localhost:9000')
-#sn = SenticNet()
-polarity=0
-feature=""
-#the number of documents of the training set, restricted to the domain D(i), in which feature C occurs
-s=0
-#index referring to the domain D(i) which the feature belongs to
+polarity = 0
+feature = ""
+# the number of documents of the training set, restricted to the domain D(i), in which feature C occurs
+s = 0
+# index referring to the domain D(i) which the feature belongs to
 index=0
 auxindex=index
 auxdoc=0
-#Define o caminho para os diretórios dos domínios de treinamento (inModel) e de teste (outModel)
+# Define o caminho para os diretórios dos domínios de treinamento (inModel) e de teste (outModel)
 inModelDir = pathlib.Path(__file__).parent.absolute().joinpath('paper-package/dranziera')
-outModelDir = pathlib.Path(__file__).parent.absolute().joinpath('paper-package/out_of_domain')
+# outModelDir = pathlib.Path(__file__).parent.absolute().joinpath('paper-package/out_of_domain')
 
-#pega o nome de cada diretório dentro do diretório Dranziera
+# pega o nome de cada diretório dentro do diretório Dranziera
 for dir in os.listdir(inModelDir):
 
-	#transforma esse nome do diretório em uma path para verificar se é um diretório mesmo 
-	#(para evitar de pegar arquivos ocultos do sistema que podem estar dentro do diretório Dranziera)
+	# transforma esse nome do diretório em uma path para verificar se é um diretório mesmo
+	# (para evitar de pegar arquivos ocultos do sistema que podem estar dentro do diretório Dranziera)
 	dirPath = os.path.join(inModelDir, dir)
 	if os.path.isdir(dirPath):
 
-		#para verificar em qual dominio estamos
+		# para verificar em qual dominio estamos
 		index+=1
 		print(index)
 		auxindex=index
 	
-		#lista todos os arquivos dentro do diretório do domínio
+		# lista todos os arquivos dentro do diretório do domínio
 		files = os.listdir(dirPath)
 		
-		#abre cada arquivo na pasta do domínio para leitura
+		# abre cada arquivo na pasta do domínio para leitura
 		for file in files:
 			
 			with open("%s/%s" % (dirPath, file)) as dataset:
 
-				#lê primeira linha do arquivo em questão e itera por cada uma das próximas linhas (cada linha é um review)
+				# lê primeira linha do arquivo em questão e itera por cada uma das próximas linhas (cada linha é um review)
 				line = dataset.readline()
 				while line:
 
-					#joga linha toda no Stanford Core NLP Toolkit
+					# joga linha toda no Stanford Core NLP Toolkit
 					coreoutput = nlp.annotate(line, properties={'annotators': 'lemma, pos, sentiment', 'outputFormat': 'json', 'timeout': 100000})
 
 					print("\rDOCUMENT: %s" % (line))
@@ -57,13 +54,13 @@ for dir in os.listdir(inModelDir):
 						print("SENTENCE: %d\n" % (sentence["index"]))
 
 						for token in sentence["tokens"]:
-							#senticnetoutput = sn.concept(line)
+							# senticnetoutput = sn.concept(line)
 							# Extração de caracteristicas- simples e complexa
-							#caso seja o primeiro doc então pega a primeira caracteristica que atende aos requisitos
+							# caso seja o primeiro doc então pega a primeira caracteristica que atende aos requisitos
 							if feature == "":
 								if token["pos"] == "RB" or token["pos"] == "RBR" or token["pos"] == "RBS" or token["pos"] == "NN" or token["pos"] == "NNS" or token["pos"] == "NNP" or token["pos"] == "VB" or token["pos"] == "VBD" or token["pos"] == "VBG" or token["pos"] == "VBN" or token["pos"] == "VBP" or token["pos"] == "JJS" or token["pos"] == "JJR" or token["pos"] == "JJ":
 									feature = token["word"]
-									#primeira ocorrência da feature no doc
+									# primeira ocorrência da feature no doc
 									s+=1
 									if sentence["sentiment"] == "Positive":
 										polarity+=1	
@@ -71,16 +68,16 @@ for dir in os.listdir(inModelDir):
 										polarity-=1
 								
 							elif feature == token["word"]:
-								#if s != auxdoc:
-									#s+=1
+								# if s != auxdoc:
+									# s+=1
 								if sentence["sentiment"] == "Positive":
 									polarity+=1	
 								elif sentence["sentiment"] == "Negative":
 									polarity-=1
 
 							print("TOKEN: %d, WORD: %s, LEMMA: %s, POS: %s, SENTIMENT: %s" % (token["index"], token["word"], token["lemma"], token["pos"], sentence["sentiment"]))
-						#with open(file +'.txt', 'w') as outfile:
-    							#json.dump(coreoutput,outfile)
+						# with open(file +'.txt', 'w') as outfile:
+    							# json.dump(coreoutput,outfile)
 						
 						print("\r")
 
@@ -95,7 +92,7 @@ for dir in os.listdir(inModelDir):
 				arquivo.write("Feature: %s, Variable S: %d,  Variable Polarity: %d, Preliminary Polarity: %d, Dominio: %s" % (feature, s, polarity, p, auxindex))
 			print("\r")
 
-		#quando o algoritmo vai para a outra pasta, significa que é um novo dominio. Então, as variaveis são reinicializadas.
+		# quando o algoritmo vai para a outra pasta, significa que é um novo dominio. Então, as variaveis são reinicializadas.
 		polarity=0
 		s=0
 		auxdoc=0
